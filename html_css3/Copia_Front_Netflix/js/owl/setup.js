71f88f1c7cbfc360629432d43671965cde49f373
roneifeix <roneifeix@gmail.com>
2022-05-22 23:30:05 -0300

    conclusão aulas

---
+++ html_css3/Copia_Front_Netflix/js/owl/setup.js
@@ -0,0 +1,46 @@
+/* $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+}) */
+var owl = $('.owl-carousel');
+
+owl.owlCarousel({
+    loop:true,
+    nav:false,
+    margin:20,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },            
+        960:{
+            items:5
+        },
+        1200:{
+            items:6
+        }
+    }
+});
+
+owl.on('mousewheel', '.owl-stage', function (e) {
+    if (e.deltaY>0) {
+        owl.trigger('next.owl');
+    } else {
+        owl.trigger('prev.owl');
+    }
+    e.preventDefault();
+});
